090ec68db9ebcadf2af8148218a9427ee1e4f338
futex <remchp@gmail.com>
2017-09-21 14:07:26 +0200

    update bitcoin.py

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -33,10 +33,11 @@ class bitcoin(BotPlugin):
 			else:
 				value = "1 bitcoin = %s EURO or %s USD. Average %s%%" % (conversion_euro, conversion_dollar, percent)
 
-			if msg.is_direct:
+			return value
+			'''if msg.is_direct:
 				self.send(msg.frm, value)
 			else:
-				self.send(msg.to, value)
+				self.send(msg.to, value)'''
 
 	@botcmd
 	def ltc(self, msg, args):
@@ -68,10 +69,138 @@ class bitcoin(BotPlugin):
 			else:
 				value = "1 Litecoin = %s EURO or %s USD or %s Bitcoin. Average %s%%" % (conversion_euro, conversion_dollar, conversion_BTC, percent)
 
-			if msg.is_direct:
-				self.send(msg.frm, value)
+			return value
+
+	@botcmd
+	def dsh(self, msg, args):
+			"""
+			Check the Dash value
+			"""
+			r =requests.get("https://min-api.cryptocompare.com/data/price?fsym=DASH&tsyms=BTC,USD,EUR", stream=True)
+			r.raise_for_status()
+
+			data = json.loads(r.content.decode('utf-8'))
+	    
+			conversion_BTC = data['BTC']
+			conversion_dollar = data['USD']
+			conversion_euro = data['EUR']
+
+			r = requests.get("https://min-api.cryptocompare.com/data/histohour?fsym=DASH&tsym=USD&limit=1", stream=True)
+
+			dataset = json.loads(r.content.decode('utf-8'))
+
+			oldValue = dataset['Data'][0]['high']
+			currentValue = dataset['Data'][1]['high']
+
+			diff =  currentValue - oldValue
+
+			percent = round(diff / currentValue * 100, 2)
+
+			if percent > 0:
+				value = "1 Dash = %s EURO or %s USD or %s Bitcoin. Average +%s%%" % (conversion_euro, conversion_dollar, conversion_BTC, percent)	
 			else:
-				self.send(msg.to, value)
+				value = "1 Dash = %s EURO or %s USD or %s Bitcoin. Average %s%%" % (conversion_euro, conversion_dollar, conversion_BTC, percent)
+
+			return value
+
+	@botcmd
+	def xrp(self, msg, args):
+			"""
+			Check the Ripple value
+			"""
+			r =requests.get("https://min-api.cryptocompare.com/data/price?fsym=XRP&tsyms=BTC,USD,EUR", stream=True)
+			r.raise_for_status()
+
+			data = json.loads(r.content.decode('utf-8'))
+	    
+			conversion_BTC = data['BTC']
+			conversion_dollar = data['USD']
+			conversion_euro = data['EUR']
+
+			r = requests.get("https://min-api.cryptocompare.com/data/histohour?fsym=XRP&tsym=USD&limit=1", stream=True)
+
+			dataset = json.loads(r.content.decode('utf-8'))
+
+			oldValue = dataset['Data'][0]['high']
+			currentValue = dataset['Data'][1]['high']
+
+			diff =  currentValue - oldValue
+
+			percent = round(diff / currentValue * 100, 2)
+
+			if percent > 0:
+				value = "1 Ripple = %s EURO or %s USD or %s Bitcoin. Average +%s%%" % (conversion_euro, conversion_dollar, conversion_BTC, percent)	
+			else:
+				value = "1 Ripple = %s EURO or %s USD or %s Bitcoin. Average %s%%" % (conversion_euro, conversion_dollar, conversion_BTC, percent)
+
+			return value
+
+	@botcmd
+	def xmr(self, msg, args):
+			"""
+			Check the Monero value
+			"""
+			r =requests.get("https://min-api.cryptocompare.com/data/price?fsym=XMR&tsyms=BTC,USD,EUR", stream=True)
+			r.raise_for_status()
+
+			data = json.loads(r.content.decode('utf-8'))
+	    
+			conversion_BTC = data['BTC']
+			conversion_dollar = data['USD']
+			conversion_euro = data['EUR']
+
+			r = requests.get("https://min-api.cryptocompare.com/data/histohour?fsym=XMR&tsym=USD&limit=1", stream=True)
+
+			dataset = json.loads(r.content.decode('utf-8'))
+
+			oldValue = dataset['Data'][0]['high']
+			currentValue = dataset['Data'][1]['high']
+
+			diff =  currentValue - oldValue
+
+			percent = round(diff / currentValue * 100, 2)
+
+			if percent > 0:
+				value = "1 Monero = %s EURO or %s USD or %s Bitcoin. Average +%s%%" % (conversion_euro, conversion_dollar, conversion_BTC, percent)	
+			else:
+				value = "1 Monero = %s EURO or %s USD or %s Bitcoin. Average %s%%" % (conversion_euro, conversion_dollar, conversion_BTC, percent)
+
+			return value
+
+
+	@botcmd
+	def doge(self, msg, args):
+			"""
+			Check the Dogecoin value
+			"""
+			r =requests.get("https://min-api.cryptocompare.com/data/price?fsym=DOGE&tsyms=BTC,USD,EUR", stream=True)
+			r.raise_for_status()
+
+			data = json.loads(r.content.decode('utf-8'))
+	    
+			conversion_BTC = data['BTC']
+			conversion_dollar = data['USD']
+			conversion_euro = data['EUR']
+
+			r = requests.get("https://min-api.cryptocompare.com/data/histohour?fsym=DOGE&tsym=USD&limit=1", stream=True)
+
+			dataset = json.loads(r.content.decode('utf-8'))
+
+			oldValue = dataset['Data'][0]['high']
+			currentValue = dataset['Data'][1]['high']
+
+			diff =  currentValue - oldValue
+
+			percent = round(diff / currentValue * 100, 2)
+
+			if percent > 0:
+				value = "1 Dogecoin = %s EURO or %s USD or %s Bitcoin. Average +%s%%" % (conversion_euro, conversion_dollar, conversion_BTC, percent)	
+			else:
+				value = "1 Dogecoin = %s EURO or %s USD or %s Bitcoin. Average %s%%" % (conversion_euro, conversion_dollar, conversion_BTC, percent)
+
+
+			return value
+
 
 	@botcmd
 	def eth(self, msg, args):
@@ -105,10 +234,7 @@ class bitcoin(BotPlugin):
 			else:
 				value = "1 Ethereum = %s EURO or %s USD or %s Bitcoin. Average %s%%" % (conversion_euro, conversion_dollar, conversion_BTC, percent)
 
-			if msg.is_direct:
-				self.send(msg.frm, value)
-			else:
-				self.send(msg.to, value)
+			return value
 
 	@botcmd
 	def zec(self, msg, args):
@@ -141,10 +267,7 @@ class bitcoin(BotPlugin):
 			else:	
 				value = "1 zcash = %s EURO or %s USD or %s Bitcoin. Average %s%%" % (conversion_euro, conversion_dollar, conversion_BTC, percent)
 
-			if msg.is_direct:
-				self.send(msg.frm, value)
-			else:
-				self.send(msg.to, value)
+			return value
 
 	@botcmd
 	def bch(self, msg, args):
@@ -177,17 +300,20 @@ class bitcoin(BotPlugin):
 			else:	
 				value = "1 Bitcoin cash = %s EURO or %s USD or %s Bitcoin. Average %s%%" % (conversion_euro, conversion_dollar, conversion_BTC, percent)
 
-			if msg.is_direct:
-				self.send(msg.frm, value)
-			else:
-				self.send(msg.to, value)
+			return value
 
 	@botcmd
 	def coincoin(self, msg, args):
-		self.bitcoin(msg, args)
-		self.bch(msg, args)
-		self.ltc(msg, args)
-		self.eth(msg, args)
-		self.zec(msg, args)
+		rtn = self.bitcoin(msg, args)
+		rtn += "\n" + self.bch(msg, args)
+		rtn += "\n" + self.ltc(msg, args)	
+		rtn += "\n" + self.eth(msg, args)
+		rtn += "\n" + self.zec(msg, args)
+		rtn += "\n" + self.dsh(msg, args)
+		rtn += "\n" + self.xmr(msg, args)
+		rtn += "\n" + self.xrp(msg, args)
+		rtn += "\n" + self.doge(msg, args)
+
+		return rtn
 
 	#https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR
